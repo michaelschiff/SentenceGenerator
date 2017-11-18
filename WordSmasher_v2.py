07834b9bbf7e1c891448116f6b5b6bc0174423cf
Michael Schiff <schiff.michael@gmail.com>
2017-11-18 02:36:53 -0800

    working on variable lookback

--- WordSmasher_v2.py
+++ WordSmasher_v2.py
@@ -28,17 +28,22 @@ def sample(mat, m):
 
 class SentenceGenerator:
     
-    def __init__(self, filepath):
-        f = open(filepath, "r")
-        self.text = f.readlines()
-        self.dictionary = set()
-        self.index = {} #word => num
-        self.rev_index = [] #num => word
-        self.probabilities()
+    def __init__(self, filepath, arity):
+        self.file = filepath
+        # each index contains the dictionary of n-tuples of size index+1
+        self.dictionaries = []
+        # each index contains a dictionary mapping a tuple of index+1 words => num
+        self.indexes = []
+        # each index contains a dictionary mapping num => a tuple of index+1 words
+        self.rev_indexes = []
+        # each index contains the matrix of arity index+1
+        self.prob_tables = []
+        self.probabilities(arity)
         
     def window(self, seq, n=2):
         "Returns a sliding window (of width n) over data from the iterable"
         "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
+        # TODO : handle window of size 1
         it = iter(seq)
         result = tuple(islice(it, n))
         if len(result) == n:
@@ -46,72 +51,59 @@ class SentenceGenerator:
         for elem in it:
             result = result[1:] + (elem,)
             yield result
-        
-    def probabilities(self):
-        for line in self.text:
-            for word in line.split(" "):
-                self.dictionary.add(word)
-        print "found " + str(len(self.dictionary)) + " unique words"
-        
-        for (word, index) in zip(self.dictionary, xrange(len(self.dictionary))):
-            self.index[word] = index
-            self.rev_index.append(word)
-        num_text = []
-        for line in self.text:
-            for word in line.split(" "):
-                num_text.append(self.index[word])
-        
-        # a dictionary, whose default value is a dictionary, whose default value is 0.
-        self.prob_table = defaultdict(lambda : defaultdict(lambda : 0))
-        for (token, next_token) in self.window(num_text): 
-            self.prob_table[token][next_token] += 1
-        normalize(self.prob_table)
 
+    def tuple_to_string(self, tup):
+        return " ".join(tup) if len(tup) > 1 else tup[0]
+        
+    # builds dictionaries, indexes, and probability tables for each arity 1..n
+    def probabilities(self, n):
+        with open(self.file, 'r') as text_file:
+            text = text_file.read().split(" ")
+            for i in xrange(0, n):
+                # for each arity, create a dictionary, indexes, and probability matrix
+                dictionary = set()
+                index = {}
+                rev_index = []
+                prob_table = defaultdict(lambda : defaultdict(lambda : 0))
+                self.dictionaries.append(dictionary)
+                self.indexes.append(index)
+                self.rev_indexes.append(rev_index)
+                self.prob_tables.append(prob_table)
+                # scan the text in tuples of size i+1
+                for word_tuple in self.window(text, i+1):
+                    dictionary.add(self.tuple_to_string(word_tuple))
+                for (token, idx) in zip(dictionary, xrange(len(dictionary))):
+                    index[token] = idx
+                    rev_index.append(token)
+                # window over i+2 because i+1 is size of tuple and we want this and the next word
+                for word_tuple in self.window(text, i+2):
+                    seed_tuple_idx = index[" ".join(word_tuple[0:-1])]
+                    # following word always comes from the arity 1 index
+                    following_word_idx = self.indexes[0][word_tuple[-1]]
+                    prob_table[seed_tuple_idx][following_word_idx] += 1
+                normalize(prob_table)
 
-    def generate_seed(self):
-        return random.sample(self.dictionary, 1)[0]
+    def generate_seed(self, arity):
+        return random.sample(self.dictionaries[arity-1], 1)[0]
     
     
     def next_word(self, seed):
         word_idx = sample(self.prob_table, self.index[seed])
         return self.rev_index[word_idx]
+
+    def sample(self, arity, current_string):
+        prob_table = self.prob_tables[arity-1]
+        current_string_idx = self.indexes[arity-1][current_string]
+        return self.rev_indexes[0][sample(prob_table, current_string_idx)]
    
-    def generate_sentence(self, length):
-        current_word = self.generate_seed()
-        res = current_word + " "
+    def generate_sentence(self, arity, length):
+        res = []
+        res.extend(self.generate_seed(arity).split(" "))
         # length-1 since the seed is the first word
         for i in xrange(0, length-1):
-            current_word_idx = sample(self.prob_table, self.index[current_word])
-            current_word = self.rev_index[current_word_idx]
-            res += current_word + " "
-        return res
-            
-
-
-    # because of the special case described above: we generate samples of the next word,
-    # until we get a non-negative index, generating a new seed each time.  We use a new seed
-    # because the seed that returned a sample of -1 will always return a sample of -1.
-    #def generate_sentence_seed(self, seed, length):
-    #    if seed not in self.dictionary:
-    #        print "I couldn't do that for you Hal"
-    #    else:
-    #        toPrint = seed+" "
-    #        while length > 0:
-    #            word_idx = -1
-    #            while word_idx < 0: 
-    #                word_idx = sample(self.prob_table, self.index[seed])
-    #                seed = self.generate_seed()
-    #            seed = self.rev_index[word_idx]
-    #            toPrint += seed + " "
-    #            length -= 1
-    #        return toPrint
-    
-    
-    #def generate_sentence(self, length):
-    #    return self.generate_sentence_seed(self.generate_seed(), length)
-        
+            res.append(self.sample(arity, " ".join(res[-1*arity:])))
+        return " ".join(res)
 
 if __name__ == "__main__":
-    x = SentenceGenerator("romeo_and_juliet.txt") 
-    print x.generate_sentence(20)
-
+    x = SentenceGenerator("romeo_and_juliet.txt", 3) 
+    print x.generate_sentence(2, 250)
